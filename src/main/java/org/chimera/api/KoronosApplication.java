59abe13c3fb45c17f4ee040b3423063e4a60f27d
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-09 01:15:22 +0300

    feat(jakarta): register Koronos API application

---
+++ src/main/java/org/chimera/api/KoronosApplication.java
@@ -0,0 +1,7 @@
+package org.chimera.api;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public final class KoronosApplication extends Application {}
